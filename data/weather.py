d9156c9358fcd7296fd3317e200f732d12866e99
C0 <103413294+SY-ML@users.noreply.github.com>
2023-01-18 22:41:46 +0900

    Weather by state

--- data/weather.py
+++ data/weather.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from meteostat import Point, Daily
+from meteostat import Point, Daily, Stations
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 from geopy import geocoders
@@ -24,56 +24,22 @@ class Countries():
         # self.UnitedStates_subdivs = pycountry.subdivisions.get(country_code=alpha2_UnitedStates)
 
     def get_ISO3166_2_subdivision_code(self, ISO3166_1_alpha_2_code):
-        data_set = pycountry.subdivisions.get(country_code= ISO3166_1_alpha_2_code)
-
+        data_set = list(pycountry.subdivisions.get(country_code= ISO3166_1_alpha_2_code))
+        # data_set = pycountry.subdivisions.get(country_code= ISO3166_1_alpha_2_code)
+        ls_subdivs = []
         for data_subdiv in data_set:
-            a = data_subdiv.replace('Subdivision(', '')
-            a = a.replace(')', '')
-            a = a.split(',')
-            print(a)
-            # print(data_subdiv)
-
-        # df = pd.DataFrame(data_set)
-        # a = [subdiv for subdiv in data_set]
-        print(data_set)
-        # print(len(data_set))
-        # a = [subdiv.split(',')[0] for subdiv in data_set]
-        # print(a)
-        # print(df)
+            ISO_3166_2_state = data_subdiv.code
+            ls_subdivs.append(ISO_3166_2_state)
+            stations = Stations().region(ISO3166_1_alpha_2_code, ISO_3166_2_state)
+            print(stations.fetch(10, sample=True))
 
 
+        # stations = Stations()
 
+        return ls_subdivs
 
-c = Countries()
+ctr = Countries()
 
-# print(c.China_subdivs)
-# print(type(c.China_subdivs))
-# print(c.UnitedStates_subdivs)
-
-
-#
-# class Weather_Data():
-#     def __init__(self):
-#         dict_cidites = {'China': ['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen', ],
-#                         'UnitedStates' : []}
-#
-#         pass
-#
-#
-#     def get_lat_and_long_with_city_name(self, city_name):
-#         geolocator = Nominatim(user_agent='SY_ML')
-#         loc = geolocator.geocode(city_name)
-#         output = {'Location': loc, 'Latitude':loc.latitude, 'Longitude':loc.longitude}
-#         return output
-#
-#
-# wd = Weather_Data()
-#
-# # a = wd.get_lat_and_long_with_city_name('Shanghai')
-# # a = wd.get_lat_and_long_with_city_name('Los Angeles')
-# # a = wd.get_lat_and_long_with_city_name('Daegu')
-# # print(a)
-#
-# g = geocoders.geonames('New York')
-# a = g.address
+print(ctr.get_ISO3166_2_subdivision_code('CN'))
+print(ctr.get_ISO3166_2_subdivision_code('US'))
 # print(a)
