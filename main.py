05242e148ef22c4d7e4d697ab6afbefd0c6b389b
C0 <103413294+SY-ML@users.noreply.github.com>
2023-01-27 15:50:07 +0900

    dataset preprocessing - merged. meteostat to be merged

--- main.py
+++ main.py
@@ -45,17 +45,45 @@ path_parquet = './archive/Historical_Product_Demand(Processed).parquet' # path o
 # # Dataset SETUP
 # Dataset_Setup() # Original dataset
 # Economic_Indicators_Setup() # Economic indicators
-Meteostat_Setup()
+# Meteostat_Setup()
 # Holidays_Setup()
 
 # cvt = Converter()
 
 # dataframe to be used for the entire analysis
-# ds = Dataset()
+ds = Dataset()
 # print(ds.df)
-# ei = Economic_Indicators()
+ei = Economic_Indicators()
 # print(ei.cci)
 # print(ei.cpi)
-# hd = Holidays()
-# print(hd.df_CN)
-# print(hd.df_US)
+hd = Holidays()
+# print(hd.calendar)
+
+"""
+Dataset preprocessing - merge
+"""
+df = ds.df
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d') # string-type date data conversion to datetime type
+
+# merge with CCI on YYYY-MM
+df = df.merge(ei.cci, left_on='Year_Month', right_on='original_period', how='left', suffixes=(None, 'CCI_'))
+df.drop(columns='original_period', inplace=True) # drop duplicated data
+df.rename(columns={'CHN':'CCI_CN', 'G-20': 'CCI_G20', 'OECD': 'CCI_OECD', 'USA':'CCI_US'}, inplace=True)
+
+# merge with CPI on YYYY-MM
+df = df.merge(ei.cpi, left_on='Year_Month', right_on='original_period', how='left')
+df.drop(columns='original_period', inplace=True) # drop duplicated data
+df.rename(columns={'CN':'CPI_CN', 'US':'CPI_US'}, inplace=True)
+
+# merge with holidays on Date column
+df_prcd = df.merge(hd.calendar, how='left', on='Date')
+print(df_prcd)
+print(df_prcd.columns)
+
+sns.heatmap(df_prcd.corr(), cmap='RdBu', vmin=-1, vmax=1, annot=True, fmt='.2f')
+plt.show()
+print(df_prcd.corr())
+exit()
+# print(df_prcd['Date'].dtype)
+# print(df_prcd['Date'])
+# print(hd.calendar['Date'].dtype)
