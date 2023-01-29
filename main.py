7baa0618e2d58e5515d0fefcab600b718d7cdae3
C0 <103413294+SY-ML@users.noreply.github.com>
2023-01-29 11:22:07 +0900

    Merge - currency weekly to be updated

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import datetime
 import numpy as np
 import pandas as pd
+
+import pandas_datareader as pdr
+from pandas_datareader import yahoo
+import pandas_datareader.data as web
+import yfinance as yf
+
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 from holidays import country_holidays
@@ -10,13 +17,15 @@ from economic_data_setup import Economic_Indicators_Setup
 from dataset_setup import Dataset_Setup
 from meteostat_setup import Meteostat_Setup
 from holidays_setup import Holidays_Setup
+from currency_setup import Currency_Setup
 
 from data.dataset import Dataset
 from data.economic_data import Economic_Indicators
 from data.holidays import Holidays
+from data.currencies import Currencies
+from data.meteostat import MeteoStats
 
 from data_processor.converter import Converter
-
 
 import matplotlib.style as style
 style.use(['seaborn'])
@@ -47,43 +56,71 @@ path_parquet = './archive/Historical_Product_Demand(Processed).parquet' # path o
 # Economic_Indicators_Setup() # Economic indicators
 # Meteostat_Setup()
 # Holidays_Setup()
-
+# Currency_Setup()
 # cvt = Converter()
 
 # dataframe to be used for the entire analysis
 ds = Dataset()
-# print(ds.df)
+
+# Product demand dataset
+df_pd = ds.df
+# print(df_pd)
+
 ei = Economic_Indicators()
-# print(ei.cci)
-# print(ei.cpi)
+
+# CCI and CPI dataset
+df_cci = ei.cci
+df_cpi = ei.cpi
+# print(df_cci)
+# print(df_cpi)
+
 hd = Holidays()
-# print(hd.calendar)
+# Chinese and American holidays
+df_hd = hd.calendar
+# print(df_hd)
+
+cy = Currencies()
+df_cny2usd = cy.cny_to_usd
+# print(df_cny2usd)
+
+ms = MeteoStats()
+df_meteo_CN = ms.cn
+# print(df_meteo_CN)
 
 """
 Dataset preprocessing - merge
 """
-df = ds.df
-df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d') # string-type date data conversion to datetime type
 
 # merge with CCI on YYYY-MM
-df = df.merge(ei.cci, left_on='Year_Month', right_on='original_period', how='left', suffixes=(None, 'CCI_'))
+df = df_pd.merge(df_cci, left_on='Year_Month', right_on='original_period', how='left')
 df.drop(columns='original_period', inplace=True) # drop duplicated data
-df.rename(columns={'CHN':'CCI_CN', 'G-20': 'CCI_G20', 'OECD': 'CCI_OECD', 'USA':'CCI_US'}, inplace=True)
 
 # merge with CPI on YYYY-MM
-df = df.merge(ei.cpi, left_on='Year_Month', right_on='original_period', how='left')
+df = df.merge(df_cpi, left_on='Year_Month', right_on='original_period', how='left')
 df.drop(columns='original_period', inplace=True) # drop duplicated data
-df.rename(columns={'CN':'CPI_CN', 'US':'CPI_US'}, inplace=True)
 
 # merge with holidays on Date column
-df_prcd = df.merge(hd.calendar, how='left', on='Date')
-print(df_prcd)
-print(df_prcd.columns)
+df = df.merge(df_hd, how='left', on='Date')
 
+# merge with CNY to USD rate
+df = df.merge(df_cny2usd, on='Date', how='left')
+
+data = yf.download('CNY=X', start = '2021-01-01', end = '2022-12-31', interval='1wk')
+# todo - 화폐 주단위로 바꿔주기
+
+print(data)
+# print(df_cny2usd.isnull().sum())
+# print(df.isnull().sum())
+# print(df[df['Open(CNY=X)'].isnull()][['Date', 'Open(CNY=X)']])
+# print(df_cny2usd.columns)
+# print(df_cny2usd)
+# merge with Chinese average temperature by region
+# df = df.merge()
+# print(df)
+# print(df.columns)
+
+
+exit()
+#
 sns.heatmap(df_prcd.corr(), cmap='RdBu', vmin=-1, vmax=1, annot=True, fmt='.2f')
 plt.show()
-print(df_prcd.corr())
-exit()
-# print(df_prcd['Date'].dtype)
-# print(df_prcd['Date'])
-# print(hd.calendar['Date'].dtype)
