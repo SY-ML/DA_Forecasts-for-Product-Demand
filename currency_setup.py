396e73c76989c4c2765670602dc65c6693dd993d
C0 <103413294+SY-ML@users.noreply.github.com>
2023-01-30 21:21:43 +0900

    currency weekly & monthly updated

--- currency_setup.py
+++ currency_setup.py
@@ -10,15 +10,36 @@ class Currency_Setup():
         make_direction(ps.path_currency_directory)
         code_cny_to_usd = 'CNY=X'
         self.start_date, self.end_date = TUPLE_DATEFROM_AND_DATETO()
-        self.load_and_save_data_with_code(code= code_cny_to_usd)
+        self.load_and_save_daily_data_with_code(code= code_cny_to_usd, basis = 'daily')
+        self.load_and_save_daily_data_with_code(code= code_cny_to_usd, basis = 'weekly')
+        self.load_and_save_daily_data_with_code(code= code_cny_to_usd, basis = 'monthly')
 
 
     def get_daily_data_with_code(self, code):
         data = yf.download(code, start = self.start_date, end = self.end_date, interval='1d')
         return data
 
-    def load_and_save_data_with_code(self, code):
-        data = self.get_daily_data_with_code(code)
-        data.to_csv(f'{ps.path_currency_directory}/{ps.currency_nameformat}_{code}({self.start_date.year}-{self.end_date.year}).csv')
+    def get_weekly_data_with_code(self, code):
+        data = yf.download(code, start = self.start_date, end = self.end_date, interval='1wk')
+        return data
+
+    def get_monthly_data_with_code(self, code):
+        data = yf.download(code, start = self.start_date, end = self.end_date, interval='1mo')
+        return data
+
+
+    def load_and_save_daily_data_with_code(self, code, basis):
+        if basis == 'daily':
+            data = self.get_daily_data_with_code(code)
+        elif basis == 'weekly':
+            data = self.get_weekly_data_with_code(code)
+        elif basis == 'monthly':
+            data = self.get_monthly_data_with_code(code)
+        else:
+            raise KeyError
+
+        data.reset_index(inplace=True)
+        data.to_csv(f'{ps.path_currency_directory}/{ps.currency_nameformat}_{basis}_{code}({self.start_date.year}-{self.end_date.year}).csv', index=False)
+
 
 
